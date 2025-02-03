f6bcd3774eccfac54b51e180da201247da8d24ec
Billy <osayebilly98@gmail.com>
2025-02-03 12:42:10 +0300

    servejjnr

--- Server/index.js
+++ Server/index.js
@@ -15,6 +15,11 @@ app.use('/server/auth', authRouter) // All requests to /server/auth will be hand
 // Regular routes and middleware
 app.use('/server/auth', authRouter)
 
+// Add a route for the root path
+app.get('/', (req, res) => {
+    res.send('Welcome to the API'); // You can customize this message
+});
+
 // Error handling middleware must be the last middleware
 app.use((err, req, res, next) => { 
     const statusCode = err.statusCode || 500;
