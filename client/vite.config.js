ca1ada7fd03841834529dfd507159d720548978c
Billy <osayebilly98@gmail.com>
2025-02-05 16:02:50 +0300

    cqCQQC

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,17 +3,17 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-
-server: {
-  proxy: {
-    '/server': {
-      target: 'http://localhost:3000',
-      secure: false,
-      changeOrigin: true,
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    proxy: {
+      '/server': {
+        target: 'http://localhost:3000',
+        secure: false,
+        changeOrigin: true,
+      },
     },
   },
-},
-
-
   plugins: [react()],
 })
